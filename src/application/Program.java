5d98a7f2dbb5fb89a8d6fc401cd9a5027f3fbd16
Marlon Henrique <matiasmarlon2003@gmail.com>
2024-10-26 10:21:14 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,17 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Departament;
+import model.entities.Seller;
+
 public class Program {
 
+	public static void main (String[] args) {
+		
+	Departament obj = new Departament(1, "books");
+	Seller seller = new Seller(21, "Bob" , "bob@gmail.com", new Date(), (double) 3000 , obj);
+	System.out.println(seller);
+	}
+	
 }
