eb9b829e6f9debbd3c43c87b718db233e991fd8a
Marlon Henrique <matiasmarlon2003@gmail.com>
2024-10-29 10:09:06 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,11 +12,12 @@ public class Program {
 	public static void main (String[] args) {
 		
 	Departament obj = new Departament(1, "books");
-	Seller seller = new Seller(21, "Bob" , "bob@gmail.com", new Date(), (double) 3000 , obj);
+	//Seller seller = new Seller(21, "Bob" , "bob@gmail.com", new Date(), (double) 3000 , obj);
 	
 	SellerDao sellerDao = DaoFactory.createSellerDao();
+	Seller seller  = sellerDao.findById(3);
 	
-	System.out.println(seller);
+	System.out.println();
 	}
 	
 }
